7814025dbc34cd458dae083cf35b4dcd12438b03
Nipul <nipulsandeepa@gmail.com>
2025-03-22 19:45:21 +0530

    working

---
+++ movie/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "d27152d43cae055931eb7a6262336af5";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
